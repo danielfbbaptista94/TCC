6abe9fb182a65fa8a612aed886e2e23126b1f12a
daniel <danielbaptista@smart4sistemas.com>
2020-12-14 16:32:26 -0300

    First Commit

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 4;
+    
+    private Rigidbody2D _rigidbody2D;
+    private Vector3 _vector3;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        _vector3 = Vector3.zero;
+        _vector3.x = Input.GetAxisRaw("Horizontal");
+        _vector3.y = Input.GetAxisRaw("Vertical");
+
+        if(_vector3 != Vector3.zero)
+        {
+            MoveCharacter();
+        }
+    }
+
+    void MoveCharacter()
+    {
+        _vector3.Normalize();
+        _rigidbody2D.MovePosition(transform.position + _vector3 * speed * Time.deltaTime);
+    }
+}
---
+++ Assets/Scripts/SceneChange.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class SceneChange : MonoBehaviour
+{
+    public static SceneChange current;
+
+    //public GameObject DialogBox;
+    //public Text DialogText;
+    //private string dialog;
+    private bool inRange = false;
+
+    private string Sala;
+    //public int id;
+
+    private void Start() {
+        Sala = gameObject.name;
+        //dialog = "Deseja entrar na sala: " + Sala + ". \n Clique em uma das opções abaixo:";
+    }
+
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D other) {
+        if(other.CompareTag("Player")) {
+
+            //if (DialogBox.activeInHierarchy)
+            //{
+            //    DialogBox.SetActive(false);
+            //} else
+            //{
+                //DialogBox.SetActive(true);
+                //DialogText.text = dialog;
+                SceneManager.LoadScene(Sala, LoadSceneMode.Single);
+            //}
+        }
+    }
+
+    
+
+    
+
+}
