0336a699afe675c241d3c34e0de4176d7993bfdd
daniel <danielbaptista@smart4sistemas.com>
2021-01-31 16:03:36 -0300

    Add Dialog no script SceneManager com opção de entrar/n entrar na Sala

--- Assets/Scripts/SceneChange.cs
+++ Assets/Scripts/SceneChange.cs
@@ -3,46 +3,55 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class SceneChange : MonoBehaviour
 {
-    public static SceneChange current;
-
-    //public GameObject DialogBox;
-    //public Text DialogText;
-    //private string dialog;
-    private bool inRange = false;
+    [SerializeField] private GameObject DialogBox;
+    [SerializeField] private TextMeshProUGUI DialogText;
+    [SerializeField] private string[] sentences;
+    private int index;
+    [SerializeField] private float typingSpeed;
 
     private string Sala;
-    //public int id;
 
     private void Start() {
         Sala = gameObject.name;
-        //dialog = "Deseja entrar na sala: " + Sala + ". \n Clique em uma das opções abaixo:";
     }
 
-    void Update()
+    IEnumerator Typing()
     {
-        
+        foreach(char letter in sentences[index].ToCharArray())
+        {
+            DialogText.text += letter;
+            yield return new WaitForSeconds(0.02f);
+        }
     }
 
     private void OnTriggerEnter2D(Collider2D other) {
         if(other.CompareTag("Player")) {
 
-            //if (DialogBox.activeInHierarchy)
-            //{
-            //    DialogBox.SetActive(false);
-            //} else
-            //{
-                //DialogBox.SetActive(true);
-                //DialogText.text = dialog;
-                SceneManager.LoadScene(Sala, LoadSceneMode.Single);
-            //}
+            if (DialogBox.activeInHierarchy)
+            {
+                DialogBox.SetActive(false);
+            }
+            else
+            {
+                DialogBox.SetActive(true);
+                StartCoroutine(Typing());
+            }
         }
     }
 
-    
+    public void ClickButtonYES()
+    {
+        SceneManager.LoadScene(Sala, LoadSceneMode.Single);
+    }
 
-    
+    public void ClickButtonNO()
+    {
+        DialogText.text = "";
+        DialogBox.SetActive(false);
+    }
 
 }
