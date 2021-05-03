bcf1f7e98b5f3eaac7e8846ee9e059e3b77dcfc9
daniel <danielbaptista@smart4sistemas.com>
2021-05-03 11:51:36 -0300

    Adicionado walking animation e modificação no script PlayerMovement

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -9,10 +9,13 @@ public class PlayerMovement : MonoBehaviour
     private Rigidbody2D _rigidbody2D;
     private Vector3 _vector3;
 
+    private Animator _animator;
+
     // Start is called before the first frame update
     void Start()
     {
         _rigidbody2D = GetComponent<Rigidbody2D>();
+        _animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -21,10 +24,21 @@ public class PlayerMovement : MonoBehaviour
         _vector3 = Vector3.zero;
         _vector3.x = Input.GetAxisRaw("Horizontal");
         _vector3.y = Input.GetAxisRaw("Vertical");
+        UpdateAnimationAndMovement();
+    }
 
-        if(_vector3 != Vector3.zero)
+    void UpdateAnimationAndMovement()
+    {
+        if (_vector3 != Vector3.zero)
         {
             MoveCharacter();
+            _animator.SetFloat("moveX", _vector3.x);
+            _animator.SetFloat("moveY", _vector3.y);
+            _animator.SetBool("moving", true);
+        }
+        else
+        {
+            _animator.SetBool("moving", false);
         }
     }
 
