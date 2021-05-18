c215e68c9a261045c80e52594111c742b77bd62e
daniel <danielbaptista@smart4sistemas.com>
2021-05-18 14:53:23 -0300

    Add Scenes and Script to PuzzleMiniGame

---
+++ Assets/Scripts/PuzzleController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PuzzleController : MonoBehaviour
+{
+    [SerializeField] private GameObject winText;
+
+    [SerializeField] private GameObject Texts;
+
+    // Use this for initialization
+    void Start()
+    {
+        winText.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (PuzzleFisico.locked && PuzzlePsicologico.locked && PuzzleVirtual.locked)
+        {
+            Texts.SetActive(false);
+            winText.SetActive(true);
+        }
+    }
+}
---
+++ Assets/Scripts/PuzzleVirtual.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PuzzleVirtual : MonoBehaviour
+{
+    [SerializeField] private Transform imagePlace;
+
+    private Vector2 initialPosition;
+
+    private Vector2 mousePosition;
+
+    private float deltaX, deltaY;
+
+    public static bool locked;
+
+    private void Start()
+    {
+        initialPosition = transform.position;
+    }
+
+    private void OnMouseDown()
+    {
+        if (!locked)
+        {
+            deltaX = Camera.main.ScreenToWorldPoint(Input.mousePosition).x - transform.position.x;
+            deltaY = Camera.main.ScreenToWorldPoint(Input.mousePosition).y - transform.position.y;
+        }
+    }
+
+    private void OnMouseDrag()
+    {
+        if (!locked)
+        {
+            mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            transform.position = new Vector2(mousePosition.x - deltaX, mousePosition.y - deltaY);
+        }
+    }
+
+    private void OnMouseUp()
+    {
+        if (Mathf.Abs(transform.position.x - imagePlace.position.x) <= 0.5f &&
+            Mathf.Abs(transform.position.y - imagePlace.position.y) <= 0.5f)
+        {
+            transform.position = new Vector2(imagePlace.position.x, imagePlace.position.y);
+            locked = true;
+        }
+        else
+        {
+            transform.position = new Vector2(initialPosition.x, initialPosition.y);
+        }
+    }
+}
